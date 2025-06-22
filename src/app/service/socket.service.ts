56dfe79082f62b74b407e057a9f594987a16d138
geekcoldhand <horatiousaharris@gmail.com>
2025-06-22 00:04:10 -0400

    added socket link to backend

---
+++ src/app/service/socket.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { io, Socket } from 'socket.io-client';
+@Injectable({
+  providedIn: 'root'
+})
+export class SocketService {
+  private socket: Socket;
+  constructor() { 
+    this.socket = io('https://chatter-box-socket.onrender.com');
+  }
+
+  public getSocket(): Socket {
+    return this.socket;
+  }
+}
