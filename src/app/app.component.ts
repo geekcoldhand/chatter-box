975e4dc6f0086ba00a846f142cccd741668ed6fc
geekcoldhand <horatiousaharris@gmail.com>
2025-06-21 23:41:25 -0400

    set up event routing for chat rooms and home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +11,14 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'chatter-box';
+
+  constructor(private router: Router) { }
+  
+  goToChat() {
+    this.router.navigate(['chat']);
+  }
+
+  goToHome() {
+    this.router.navigate(['']);
+  }
 }
